f871312de674d744f3590f61e6289a49b6172b51
Kyle McGuffin <kyle.mcguffin@vumc.org>
2022-02-24 15:58:37 -0500

    Convert image to being loaded into a canvas Calculate x,y coordinates from config and age/head circumference Print a red circle around the coordinates for the patient's age/head circumference Prevent images from displaying unless all configuration is completed and all the needed data is available

--- HeadCircChart.php
+++ HeadCircChart.php
@@ -17,11 +17,63 @@ class HeadCircChart extends AbstractExternalModule
 			$chartInstrument = $this->getProject()->getFormForField($chartField);
 			
 			if($chartInstrument == $instrument) {
-				echo "<script type='text/javascript' src='".$this->getUrl("js/functions.js")."'></script>
-				<script type='text/javascript'>
-						var headCircImagePath = '".$this->getUrl("image.php")."';
-						$(document).ready(function() { insertImageChart('girls','".$chartField."'); });
-				</script>";
+				$age = false;
+				$sex = false;
+				$circumference = false;
+				$chartType = false;
+				
+				$recordData = \REDCap::getData([
+					"records" => $record,
+					"project_id" => $project_id,
+					"fields" => [$sexField,$ageField,$circumferenceField,$this->getProject()->getRecordIdField()],
+					"return_format" => "json",
+					"events" => $event_id
+				]);
+				$recordData = json_decode($recordData,true);
+				
+				foreach($recordData as $eventDetails) {
+					if($eventDetails[$sexField] !== "") {
+						$sex = $eventDetails[$sexField];
+					}
+					
+					if(!array_key_exists("redcap_repeat_instance") ||
+								($eventDetails["redcap_repeat_instance"] == $repeat_instance &&
+								$eventDetails["redcap_repeat_instrument"] == $instrument)) {
+						$age = $eventDetails[$ageField];
+						$circumference = $eventDetails[$circumferenceField];
+					}
+				}
+				
+				## Calculate x,y coordinates of mark
+				if($sex === "0") {
+					$chartType = "girls";
+				}
+				elseif($sex === "1") {
+					$chartType = "boys";
+				}
+				
+				if($age && $circumference && $chartType) {
+					$startX = $this->getProjectSetting("x-offset-$chartType");
+					$startY = $this->getProjectSetting("y-offset-$chartType");
+					$xWidth = $this->getProjectSetting("x-width-$chartType");
+					$yWidth = $this->getProjectSetting("y-width-$chartType");
+					$ageStart = $this->getProjectSetting("x-start-age");
+					$ageRange = $this->getProjectSetting("x-end-age");
+					$headStart = $this->getProjectSetting("y-start-head");
+					$headRange = $this->getProjectSetting("y-end-head");
+					
+					if($startX && $startY && $xWidth && $yWidth && $ageStart && $ageRange && $headStart && $headRange) {
+						$x = $startX + ($xWidth / ($ageRange - $ageStart)) * ($age - $ageStart);
+						$y = $startY + ($yWidth / ($headRange - $headStart)) * ($circumference - $headStart);
+						
+						echo "<script type='text/javascript' src='".$this->getUrl("js/functions.js")."'></script>
+							<script type='text/javascript'>
+									var headCircImagePath = '".$this->getUrl("image.php")."';
+									$(document).ready(function() { insertImageChart('girls','".$chartField."',$x,$y); });
+							</script>";
+					}
+				}
+				
 			}
 		}
 	}
--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,20 @@
-function insertImageChart(type,field) {
-    $("#" + field + "-tr").after(
-        "<tr><td class='labelrc' colspan='2'><image src='" + headCircImagePath + "&type=" + type + "' /></td></tr>"
-    );
+function insertImageChart(type,field,x,y) {
+    let image = new Image();
+    image.src = headCircImagePath + "&type=" + type;
+    image.onload = function() {
+        $("#" + field + "-tr").after(
+            "<tr><td class='labelrc' colspan='2'>" +
+            "<canvas id='headCircCanvas' height='" + image.height + "' width='" + image.width + "' />" +
+            "</td></tr>"
+        );
+        let canvas = document.getElementById("headCircCanvas");
+        let context = canvas.getContext('2d');
+        context.drawImage(image,0,0);
+        context.strokeStyle = "#FF0000";
+        context.lineWidth = 3;
+        context.beginPath();
+        context.arc(x, image.height - y, 10, 0, 2 * Math.PI);
+        context.stroke();
+    }
+
 }
